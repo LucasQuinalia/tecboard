b57b393fa24c4d238efe942b9976437005d9f210
lucasquinalia <lrgquinalia@gmail.com>
2025-09-12 18:04:43 -0300

    banner and card creations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
 import './App.css'
+import {Tema} from './componentes/Tema'
 import { FormularioDeEvento } from './componentes/FormularioDeEvento'
+import { Banner } from './componentes/Banner'
+import { CardEvento } from './componentes/CardEvento'
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
+  const eventos = [
+    {
+      capa: 'https://raw.githubusercontent.com/viniciosneves/techboard-assets/refs/heads/main/imagem_1.png',
+      tema: temas[0],
+      data: new Date(),
+      titulo: 'Mulheres no Front'
+    }
+  ]
 
 function App() {
   return (
@@ -7,10 +46,16 @@ function App() {
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner/>
       <FormularioDeEvento/>
+      {temas.map(function(item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item}/>
+            <CardEvento evento={eventos[0]}/>
+          </section>
+        )
+      })}
     </main>
   )
 }
